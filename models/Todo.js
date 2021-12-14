0460817d192fcb83d685cbfccf2642281ef858e6
Ricky Aston <astonmedia@hotmail.com>
2021-12-15 08:16:12 +1000

    added error handling and async error

---
+++ models/Todo.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { default: slugify } = require("slugify");
+const sulgify = require("slugify");
+
+const TodoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please ensure a todo has a title"],
+    trim: true,
+    maxlength: [100, "title cannot be more than 100 characters"],
+  },
+  slug: String,
+  details: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+TodoSchema.pre("save", function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  next();
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
