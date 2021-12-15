ea493aa08a704c806c96238b9654be7bcff52fb6
Ricky Aston <astonmedia@hotmail.com>
2021-12-16 07:00:15 +1000

    added seeder.js

---
+++ seeder.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const colors = require("colors");
+
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+// Load Models
+const Todos = require("./models/Todo");
+const Todo = require("./models/Todo");
+
+// Read Json files
+const todos = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/todos.json`, "utf-8")
+);
+
+// Import into database
+const importData = async () => {
+  try {
+    await Todo.create(todos);
+    console.log("Data imported...".green.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Todo.deleteMany();
+    console.log("Data Destroyed...".red.inverse);
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
